8f87eb2be920f8886d36066d3f51f9c73d1bc561
zac <zwgagne@axso.io>
2023-12-01 08:07:06 -0500

    initial setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# Advent of code 2023 
+
+Repo listing all my solutions for [Advent of code](https://adventofcode.com/) 2023 in Typescript!
+
+
+## Credits
+
+Original setup taken from https://github.com/LBognanni/adventofcode-typescript-starter, it may be slightly evolved.
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { Day } from './day';
+import day0 from './day0/index';
+import day1 from './day1/index';
+import day2 from './day2/index';
+import day3 from './day3/index';
+import day4 from './day4/index';
+// MORE IMPORTS HERE
+const days: Day[] = [
+    day0,
+    day1,
+    day2,
+    day3,
+    day4,
+    // MORE DAYS HERE
+];
+
+async function runDay(dayId: number) {
+  const resultPart1 = await days[dayId].partOne();
+  console.log("Part 1 result:\n");
+  console.log(resultPart1);
+
+  console.log("\n");
+
+  const resultPart2 = await days[dayId].partTwo();
+  console.log("Part 2 result:\n");
+  console.log(resultPart2);
+}
+
+console.log("\n\n\n   ADVENT OF CODE 2021 \n\n");
+const params = process.argv.splice(2);
+if (params.length) {
+  runDay(parseInt(params[0], 10));
+} else {
+  console.log(`Usage: npm run start [day]`);
+  console.log(`Available days: [ ${days.map((x) => x.id).join(", ")} ]`);
+}
